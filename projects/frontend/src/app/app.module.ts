e16110b1e0f20262a5be1308198b154848f3520a
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2019-02-18 11:56:09 +0100

    chore: add Angular Material package

--- projects/frontend/src/app/app.module.ts
+++ projects/frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 
@@ -12,6 +13,7 @@ import {AppComponent} from './app.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
