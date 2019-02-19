8d959444b511a195f7bbb84b817a63cc20f7bb98
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-02-19 09:03:56 +0100

    feat(web-api): change relatishion maingame -> game

--- projects/web-api/Thinktecture.Boardist.WebApi/Database/BoardistContext.cs
+++ projects/web-api/Thinktecture.Boardist.WebApi/Database/BoardistContext.cs
@@ -24,52 +24,32 @@ namespace Thinktecture.Boardist.WebApi.Database
     {
       base.OnModelCreating(modelBuilder);
 
-      modelBuilder.Entity<Category>().HasKey(p => p.Id);
-      modelBuilder.Entity<Category>().Property(p => p.Name).HasMaxLength(MAX_STRING_LENGTH);
+      CreateCategoryModel(modelBuilder);
+      CreateGameModel(modelBuilder);
+      CreateGameAuthorModel(modelBuilder);
+      CreateGameIllustrator(modelBuilder);
+      CreateGameCategoryModel(modelBuilder);
+      CreatePersonModel(modelBuilder);
+      CreatePublisherModel(modelBuilder);
 
-      modelBuilder.Entity<Game>().HasKey(p => p.Id);
-      modelBuilder.Entity<Game>().Property(p => p.Name).HasMaxLength(MAX_STRING_LENGTH);
+      SeedDatabase(modelBuilder);
+    }
 
-      modelBuilder.Entity<Game>()
-        .HasOne(p => p.Publisher)
-        .WithMany()
-        .HasForeignKey(p => p.PublisherId)
-        .IsRequired();
+    private static void CreatePublisherModel(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Publisher>().HasKey(p => p.Id);
+      modelBuilder.Entity<Publisher>().Property(p => p.Name).HasMaxLength(MAX_STRING_LENGTH);
+    }
 
-      modelBuilder.Entity<Game>()
-        .HasOne(p => p.MainGame)
-        .WithOne()
-        .HasForeignKey<Game>(p => p.MainGameId)
-        .IsRequired(false);
+    private static void CreatePersonModel(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Person>().HasKey(p => p.Id);
+      modelBuilder.Entity<Person>().Property(p => p.FirstName).HasMaxLength(MAX_STRING_LENGTH);
+      modelBuilder.Entity<Person>().Property(p => p.LastName).HasMaxLength(MAX_STRING_LENGTH);
+    }
 
-      modelBuilder.Entity<Game>().HasMany(p => p.Categories).WithOne();
-
-      modelBuilder.Entity<Game>().Property(p => p.BuyPrice).HasColumnType("decimal(3,2)");
-
-      modelBuilder.Entity<GameAuthor>().HasKey(p => new {p.GameId, p.AuthorId});
-
-      modelBuilder.Entity<GameAuthor>()
-        .HasOne(p => p.Game)
-        .WithMany(p => p.Authors)
-        .HasForeignKey(p => p.GameId);
-
-      modelBuilder.Entity<GameAuthor>()
-        .HasOne(p => p.Author)
-        .WithMany()
-        .HasForeignKey(p => p.AuthorId);
-
-      modelBuilder.Entity<GameIllustrator>().HasKey(p => new {p.GameId, p.IllustratorId});
-
-      modelBuilder.Entity<GameIllustrator>()
-        .HasOne(p => p.Game)
-        .WithMany(p => p.Illustrators)
-        .HasForeignKey(p => p.GameId);
-
-      modelBuilder.Entity<GameIllustrator>()
-        .HasOne(p => p.Illustrator)
-        .WithMany()
-        .HasForeignKey(p => p.IllustratorId);
-      
+    private static void CreateGameCategoryModel(ModelBuilder modelBuilder)
+    {
       modelBuilder.Entity<GameCategory>().HasKey(p => new {p.GameId, p.CategoryId});
 
       modelBuilder.Entity<GameCategory>()
@@ -81,17 +61,66 @@ namespace Thinktecture.Boardist.WebApi.Database
         .HasOne(p => p.Category)
         .WithMany()
         .HasForeignKey(p => p.CategoryId);
-
-      modelBuilder.Entity<Person>().HasKey(p => p.Id);
-      modelBuilder.Entity<Person>().Property(p => p.FirstName).HasMaxLength(MAX_STRING_LENGTH);
-      modelBuilder.Entity<Person>().Property(p => p.LastName).HasMaxLength(MAX_STRING_LENGTH);
-
-      modelBuilder.Entity<Publisher>().HasKey(p => p.Id);
-      modelBuilder.Entity<Publisher>().Property(p => p.Name).HasMaxLength(MAX_STRING_LENGTH);
-
-      SeedDatabase(modelBuilder);
     }
-   
+
+    private static void CreateGameIllustrator(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<GameIllustrator>().HasKey(p => new {p.GameId, p.IllustratorId});
+
+      modelBuilder.Entity<GameIllustrator>()
+        .HasOne(p => p.Game)
+        .WithMany(p => p.Illustrators)
+        .HasForeignKey(p => p.GameId);
+
+      modelBuilder.Entity<GameIllustrator>()
+        .HasOne(p => p.Illustrator)
+        .WithMany()
+        .HasForeignKey(p => p.IllustratorId);
+    }
+
+    private static void CreateGameAuthorModel(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<GameAuthor>().HasKey(p => new {p.GameId, p.AuthorId});
+
+      modelBuilder.Entity<GameAuthor>()
+        .HasOne(p => p.Game)
+        .WithMany(p => p.Authors)
+        .HasForeignKey(p => p.GameId);
+
+      modelBuilder.Entity<GameAuthor>()
+        .HasOne(p => p.Author)
+        .WithMany()
+        .HasForeignKey(p => p.AuthorId);
+    }
+
+    private static void CreateGameModel(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Game>().HasKey(p => p.Id);
+      modelBuilder.Entity<Game>().Property(p => p.Name).HasMaxLength(MAX_STRING_LENGTH);
+
+      modelBuilder.Entity<Game>()
+        .HasOne(p => p.Publisher)
+        .WithMany()
+        .HasForeignKey(p => p.PublisherId)
+        .IsRequired();
+
+      modelBuilder.Entity<Game>()
+        .HasOne(p => p.MainGame)
+        .WithMany()
+        .HasForeignKey(p => p.MainGameId)
+        .IsRequired(false);
+
+      modelBuilder.Entity<Game>().HasMany(p => p.Categories).WithOne();
+
+      modelBuilder.Entity<Game>().Property(p => p.BuyPrice).HasColumnType("decimal(3,2)");
+    }
+
+    private static void CreateCategoryModel(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Category>().HasKey(p => p.Id);
+      modelBuilder.Entity<Category>().Property(p => p.Name).HasMaxLength(MAX_STRING_LENGTH);
+    }
+
     private void SeedDatabase(ModelBuilder modelBuilder)
     {
       var persons = new List<Person>()
---
+++ projects/web-api/Thinktecture.Boardist.WebApi/Migrations/20190219075846_ChangeRelationshipForMainGameToGame.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Thinktecture.Boardist.WebApi.Migrations
+{
+    public partial class ChangeRelationshipForMainGameToGame : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Games_MainGameId",
+                table: "Games");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_MainGameId",
+                table: "Games",
+                column: "MainGameId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Games_MainGameId",
+                table: "Games");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_MainGameId",
+                table: "Games",
+                column: "MainGameId",
+                unique: true,
+                filter: "[MainGameId] IS NOT NULL");
+        }
+    }
+}
