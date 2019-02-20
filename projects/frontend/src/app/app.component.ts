2944e2caac41930a12e36099fd671244fb4a461f
Thomas Hilzendegen <thomas.hilzendegen@thinktecture.com>
2019-02-20 16:17:47 +0100

    fix(build): use methods instead observable because of runtime exception

--- projects/frontend/src/app/app.component.ts
+++ projects/frontend/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {MediaMatcher} from '@angular/cdk/layout';
 import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {MatSidenav} from '@angular/material';
-import {fromEvent, Subscription} from 'rxjs';
 
 @Component({
   selector: 'ttb-root',
@@ -13,20 +12,20 @@ import {fromEvent, Subscription} from 'rxjs';
   },
 })
 export class AppComponent implements OnInit, OnDestroy {
-  private subscription = Subscription.EMPTY;
-
   mobileQuery: MediaQueryList;
   @ViewChild(MatSidenav) snav: MatSidenav;
+
+  private listener = () => undefined;
 
   constructor(private readonly media: MediaMatcher) {
   }
 
   ngOnInit(): void {
     this.mobileQuery = this.media.matchMedia('(max-width: 600px)');
-    this.subscription = fromEvent(this.mobileQuery, 'change').subscribe();
+    this.mobileQuery.addListener(this.listener);
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.mobileQuery.removeListener(this.listener);
   }
 }
