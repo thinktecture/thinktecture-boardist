db85a43a1581f533cee3ad80ef3dddc9656a8889
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-03-12 12:42:53 +0100

    fix(offline): use correct API endpoint

--- projects/frontend/src/environments/environment.prod.ts
+++ projects/frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseApiUrl: 'https://tt-boardist-api.azurewebsites.net/api/',
+  baseApiUrl: 'https://tt-boardist-api-offline.azurewebsites.net/api/',
   syncStartDelay: 1_000,
   syncCheckInterval: 60_000,
   syncRetry: 3,
