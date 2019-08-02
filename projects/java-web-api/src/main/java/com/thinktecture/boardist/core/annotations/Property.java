6e415510df064c28086ade7023f6a32fa54949e2
Max Schulte <max.schulte@thinktecture.com>
2019-08-02 16:47:15 +0200

    feat: application.property injection

---
+++ projects/java-web-api/src/main/java/com/thinktecture/boardist/core/annotations/Property.java
@@ -0,0 +1,21 @@
+package com.thinktecture.boardist.core.annotations;
+
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+/**
+ * Represents an property key to be injected
+ */
+@Qualifier
+@Retention(RUNTIME)
+@Target({TYPE, METHOD, FIELD, PARAMETER})
+public @interface Property {
+    @Nonbinding String value() default "";
+    @Nonbinding boolean required() default true;
+}
