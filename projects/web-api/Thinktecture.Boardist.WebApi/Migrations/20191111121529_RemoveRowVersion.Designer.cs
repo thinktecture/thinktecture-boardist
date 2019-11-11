13b08530a238db01ff81d0234eb30469a68bcba8
Manuel Rauber <manuel.rauber@thinktecture.com>
2019-11-11 13:15:40 +0100

    fix: remove row version

--- projects/web-api/Thinktecture.Boardist.WebApi/Migrations/20191111121529_RemoveRowVersion.Designer.cs
+++ projects/web-api/Thinktecture.Boardist.WebApi/Migrations/20191111121529_RemoveRowVersion.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Thinktecture.Boardist.WebApi.Database;
 
 namespace Thinktecture.Boardist.WebApi.Migrations
 {
     [DbContext(typeof(BoardistContext))]
-    partial class BoardistContextModelSnapshot : ModelSnapshot
+    [Migration("20191111121529_RemoveRowVersion")]
+    partial class RemoveRowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
